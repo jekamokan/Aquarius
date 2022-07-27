2e2bcb0d509a5d2bb752b07ab894f3a9e4522c97
jekamokan <jekamokan1994@gmail.com>
2022-07-27 23:30:33 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.project__slider').slick({
+        arrows:false,
+        dots:true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        variableWidth: true,
+        autoplay:false,
+        autoplaySpeed:1500
+    });
+});
